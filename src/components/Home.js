a12f71180092adb40b0e07c2de647ca9a047570f
IsthmusAlien <131596732+IsthmusAlien@users.noreply.github.com>
2024-12-31 20:00:37 +0530

    Ready or not world, but new God is open for business

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,21 @@
 import React from 'react'
 import "./css/Home.css"
-import Map from './Map'
+import Main from './Main'
 
 export default function Home() {
     return (
         <main>
             <div className="simulationCon">
-                <div className="mapsCon">
-                    <Map />
-                </div>
 
                 <div className="instructionCon">
-
+                    Instructions go here!
                 </div>
+                
+                <div className="mainCon">
+                    <Main />
+                </div>
+
             </div>
         </main>
-    )
+    );
 }
