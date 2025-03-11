890d947e14da512846129602f347e08940f14ecf
IsthmusAlien <131596732+IsthmusAlien@users.noreply.github.com>
2025-03-11 09:53:32 +0530

    Procedure Added

--- src/components/Procedure.js
+++ src/components/Procedure.js
@@ -10,79 +10,76 @@ export default function Procedure() {
                 <li>
                     Press the "Left Arrow" / "&lt;" to hide the Instructions Panel (Refer Fig. 1). Similary press the "Right Arrow" / "&gt;" to show the Instructions Panel (Refer Fig.2).
                     <img src="/procedure_imgs/1_(1).png" alt="Activity 1 Image 1" />
-                    <strong>Fig. 1: Activity 1 Image 1</strong>
+                    <strong>Fig. 1</strong>
                 </li>
                 <li>
-                    Press "Zoom In" / "+" to zoom in and "Zoom Out" / "-" to zoom out, alternatively "Scroll Wheel" can also be used (Refer Fig. 2).
+                    Press "Zoom In" / "+" to zoom in and "Zoom Out" / "-" to zoom out, alternatively "Scroll Wheel" and "Touch" can also be used (Refer Fig. 2).
                     <img src="/procedure_imgs/1_(2).png" alt="Activity 1 Image 2" />
-                    <strong>Fig. 2: Activity 1 Image 2</strong>
+                    <strong>Fig. 2</strong>
                 </li>
                 <li>
-                    The bottom section displays correctly matched pairs.  
+                    Pressing "Draw" / "✎" will enable draw mode and pressing it again will disable it. Hold "Mouse 1" or "Touch" and hover over the area to draw.                    
                     <img src="/procedure_imgs/1_(3).png" alt="Activity 1 Image 3" />
-                    <strong>Fig. 3: Activity 1 Image 3</strong>
+                    <strong>Fig. 3</strong>
                 </li>
                 <li>
-                    The bottom section displays correctly matched pairs.  
+                    Press "Mouse 2" or "Undo" / "↶" to undo the previous stroke.
                     <img src="/procedure_imgs/1_(4).png" alt="Activity 1 Image 4" />
-                    <strong>Fig. 4: Matched pairs</strong>
+                    <strong>Fig. 4</strong>
                 </li>
                 <li>
-                    The bottom section displays correctly matched pairs.  
+                    Hold "Mouse 3" or Press "Pause" / "❚❚" to pause draw mode and move the map freely. Similary release "Mouse 3" or Press "Resume" / "▶" to resume draw mode.
                     <img src="/procedure_imgs/1_(5).png" alt="Activity 1 Image 5" />
-                    <strong>Fig. 5: Matched pairs</strong>
+                    <strong>Fig. 5</strong>
                 </li>
                 <li>
-                    The bottom section displays correctly matched pairs.  
+                    Slide the "Opacity Slide" in the Top Right Corner to adjust opacity of the reference plate.  
                     <img src="/procedure_imgs/1_(6).png" alt="Activity 1 Image 6" />
-                    <strong>Fig. 6: Matched pairs</strong>
+                    <strong>Fig. 6</strong>
                 </li>
                 <li>
-                    The bottom section displays correctly matched pairs.  
+                    Press "View" / "⇄" to change the view to the map.  
                     <img src="/procedure_imgs/1_(7).png" alt="Activity 1 Image 7" />
-                    <strong>Fig. 7: Matched pairs</strong>
+                    <strong>Fig. 7</strong>
                 </li>
                 <li>
-                    The bottom section displays correctly matched pairs.  
+                    Press "Compare" / "𓍝" to get result.  
                     <img src="/procedure_imgs/1_(8).png" alt="Activity 1 Image 8" />
-                    <strong>Fig. 8: Matched pairs</strong>
+                    <strong>Fig. 8</strong>
                 </li>
-                <li>Click on 'NEXT' to proceed to part 2 of the lab.</li>
             </ol>
 
             <h3>Activiy 2</h3>
             <ol>
                 <li>
-                    The bottom section displays correctly matched pairs.  
+                    Use "Up" / "▲" and "Down" / "▼" to sort between names.
                     <img src="/procedure_imgs/2_(1).png" alt="Activity 2 Image 1" />
-                    <strong>Fig. 1: Activity 2 Image 1</strong>
+                    <strong>Fig. 1</strong>
                 </li>
                 <li>
-                    The bottom section displays correctly matched pairs.  
+                    Drag the text to their appropriate location using "Mouse 1" or "Touch".  
                     <img src="/procedure_imgs/2_(2).png" alt="Activity 2 Image 2" />
-                    <strong>Fig. 2: Activity 2 Image 2</strong>
+                    <strong>Fig. 2</strong>
                 </li>
                 <li>
-                    The bottom section displays correctly matched pairs.  
+                    Press "Compare" / "𓍝" to get result. 
                     <img src="/procedure_imgs/2_(3).png" alt="Activity 2 Image 3" />
-                    <strong>Fig. 2: Activity 3 Image 3</strong>
+                    <strong>Fig. 3</strong>
                 </li>
-                <li>For reference, sea port locations are indicated on the map by a ship symbol.</li>
             </ol>
 
             <h3>Guide</h3>
             <ol>
                 <li>
-                    The bottom section displays correctly matched pairs.  
+                    Hover over or Press the polygons of Mountain Peaks and Ranges using "Mouse 1" or "Touch".  
                     <img src="/procedure_imgs/3_(1).png" alt="Activity 3 Image 1" />
-                    <strong>Fig. 1: Activity 3 Image 1</strong>
+                    <strong>Fig. 1</strong>
                 </li>
                 <li>
-                    The bottom section displays correctly matched pairs.  
+                    The Description contains Geographical Information regarding the Respective Mountain Peak or Range.  
                     <img src="/procedure_imgs/3_(2).png" alt="Activity 3 Image 2" />
-                    <strong>Fig. 2: Activity 3 Image 2</strong>
+                    <strong>Fig. 2</strong>
                 </li>
-                <li>Once all the sea port names are selected, click on "SUBMIT" to get the feedback.</li>
             </ol>
         </div>
   );
