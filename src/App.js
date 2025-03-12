665c0e4b85cf49715fad5c45e3395c4792067084
IsthmusAlien <131596732+IsthmusAlien@users.noreply.github.com>
2025-03-12 11:28:27 +0530

    Youtube was not working

--- src/App.js
+++ src/App.js
@@ -63,16 +63,7 @@ function App() {
               path="/animation"
               element={
                 <div className="video-container">
-                  <iframe
-                    width="80%"
-                    height="100%"
-                    src="https://www.youtube.com/embed/Y1HOrfks1c4?si=H33yoaC4ez5uORVr"
-                    title="YouTube video player"
-                    frameBorder="0"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                    referrerPolicy="strict-origin-when-cross-origin"
-                    allowFullScreen
-                  ></iframe>
+<iframe width="80%" height="100%" src="https://www.youtube.com/embed/Y1HOrfks1c4?si=aaIfgu99YdQAJLlB" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
                 </div>
               }
             />
--- src/components/Procedure.js
+++ src/components/Procedure.js
@@ -8,7 +8,7 @@ export default function Procedure() {
             <h3>Activiy 1</h3>
             <ol>
                 <li>
-                    Press the "Left Arrow" / "&lt;" to hide the Instructions Panel (Refer Fig. 1). Similary press the "Right Arrow" / "&gt;" to show the Instructions Panel (Refer Fig.2).
+                    Press the "Left Arrow" / "&lt;" to hide the Instructions Panel (Refer Fig. 1). Similarly press the "Right Arrow" / "&gt;" to show the Instructions Panel (Refer Fig.2).
                     <img src="/procedure_imgs/1_(1).png" alt="Activity 1 Image 1" />
                     <strong>Fig. 1</strong>
                 </li>
@@ -18,32 +18,32 @@ export default function Procedure() {
                     <strong>Fig. 2</strong>
                 </li>
                 <li>
-                    Pressing "Draw" / "✎" will enable draw mode and pressing it again will disable it. Hold "Mouse 1" or "Touch" and hover over the area to draw.                    
+                    Pressing "Draw" / "✎" will enable draw mode and pressing it again will disable it. Hold "Mouse 1" or "Touch" and hover over the area to draw (Refer Fig. 3).                  
                     <img src="/procedure_imgs/1_(3).png" alt="Activity 1 Image 3" />
                     <strong>Fig. 3</strong>
                 </li>
                 <li>
-                    Press "Mouse 2" or "Undo" / "↶" to undo the previous stroke.
+                    Press "Mouse 2" or "Undo" / "↶" to undo the previous stroke (Refer Fig. 4).
                     <img src="/procedure_imgs/1_(4).png" alt="Activity 1 Image 4" />
                     <strong>Fig. 4</strong>
                 </li>
                 <li>
-                    Hold "Mouse 3" or Press "Pause" / "❚❚" to pause draw mode and move the map freely. Similary release "Mouse 3" or Press "Resume" / "▶" to resume draw mode.
+                    Hold "Mouse 3" or Press "Pause" / "❚❚" to pause draw mode and move the map freely. Similary release "Mouse 3" or Press "Resume" / "▶" to resume draw mode (Refer Fig. 4 and Fig. 5).
                     <img src="/procedure_imgs/1_(5).png" alt="Activity 1 Image 5" />
                     <strong>Fig. 5</strong>
                 </li>
                 <li>
-                    Slide the "Opacity Slide" in the Top Right Corner to adjust opacity of the reference plate.  
+                    Slide the "Opacity Slide" in the Top Right Corner to adjust opacity of the reference plate (Refer Fig. 6).
                     <img src="/procedure_imgs/1_(6).png" alt="Activity 1 Image 6" />
                     <strong>Fig. 6</strong>
                 </li>
                 <li>
-                    Press "View" / "⇄" to change the view to the map.  
+                    Press "View" / "⇄" to change the view to the map (Refer Fig. 7).
                     <img src="/procedure_imgs/1_(7).png" alt="Activity 1 Image 7" />
                     <strong>Fig. 7</strong>
                 </li>
                 <li>
-                    Press "Compare" / "𓍝" to get result.  
+                    Press "Compare" / "𓍝" to get result  (Refer Fig. 8).  
                     <img src="/procedure_imgs/1_(8).png" alt="Activity 1 Image 8" />
                     <strong>Fig. 8</strong>
                 </li>
@@ -52,17 +52,17 @@ export default function Procedure() {
             <h3>Activiy 2</h3>
             <ol>
                 <li>
-                    Use "Up" / "▲" and "Down" / "▼" to sort between names.
+                    Use "Up" / "▲" and "Down" / "▼" to sort between names  (Refer Fig. 1).
                     <img src="/procedure_imgs/2_(1).png" alt="Activity 2 Image 1" />
                     <strong>Fig. 1</strong>
                 </li>
                 <li>
-                    Drag the text to their appropriate location using "Mouse 1" or "Touch".  
+                    Drag the text to their appropriate location using "Mouse 1" or "Touch" (Refer Fig. 2). 
                     <img src="/procedure_imgs/2_(2).png" alt="Activity 2 Image 2" />
                     <strong>Fig. 2</strong>
                 </li>
                 <li>
-                    Press "Compare" / "𓍝" to get result. 
+                    Press "Compare" / "𓍝" to get result (Refer Fig. 3).
                     <img src="/procedure_imgs/2_(3).png" alt="Activity 2 Image 3" />
                     <strong>Fig. 3</strong>
                 </li>
@@ -71,12 +71,12 @@ export default function Procedure() {
             <h3>Guide</h3>
             <ol>
                 <li>
-                    Hover over or Press the polygons of Mountain Peaks and Ranges using "Mouse 1" or "Touch".  
+                    Hover over or Press the polygons of Mountain Peaks and Ranges using "Mouse 1" or "Touch" (Refer Fig. 1).  
                     <img src="/procedure_imgs/3_(1).png" alt="Activity 3 Image 1" />
                     <strong>Fig. 1</strong>
                 </li>
                 <li>
-                    The Description contains Geographical Information regarding the Respective Mountain Peak or Range.  
+                    The Description contains Geographical Information regarding the Respective Mountain Peak or Range (Refer Fig. 2).  
                     <img src="/procedure_imgs/3_(2).png" alt="Activity 3 Image 2" />
                     <strong>Fig. 2</strong>
                 </li>
